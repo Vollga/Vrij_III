0fb8f3273e5df9dbd1566ae544b67e5457bf1234
Carl <jancarlpeters@gmail.com>
2021-01-05 22:23:10 +0100

    FLOORRRRSSSS!!!

---
+++ Dungeon Disasters/Assets/Scripts/TileRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.transform.rotation = Quaternion.Euler(0, Random.Range(0,4)*90, 0);
+    }
+
+
+}
