51fbf73632f86b8c69284fbbbd708a1ccdf874f7
Vollga <jancarlpeters@gmail.com>
2021-01-26 11:57:02 +0100

    additional camera functions

--- Dungeon Disasters/Assets/Scripts/TopDownCam.cs
+++ Dungeon Disasters/Assets/Scripts/TopDownCam.cs
@@ -8,6 +8,8 @@ public class TopDownCam : MonoBehaviour
     
     [Range(0.0f, 1.0f)]
     public float cameraSpeed = 1;
+    public bool _rightStickControlsZoom = false;
+    public float zoomStrength = 2;
     public float maxJoystickOffset = 1;
 
     [Header("Offset")]
@@ -15,19 +17,29 @@ public class TopDownCam : MonoBehaviour
     public Vector3 offsetAngle = new Vector3(70, 0,0);
     
     Transform target;
+    Camera cam;
     private Vector3 velocity = Vector3.zero;
     private Vector3 joystickPos = Vector3.zero;
+    private float startFov;
 
     private void Start()
     {
+        cam = this.GetComponent<Camera>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         transform.position = new Vector3(target.position.x,0,target.position.z) + offsetPos;
+        startFov = cam.fieldOfView;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        joystickPos = new Vector3(Input.GetAxis("HorizontalR"), 0, Input.GetAxis("VerticalR")) * maxJoystickOffset;
+        if (_rightStickControlsZoom)
+        {
+            cam.fieldOfView = startFov + (Input.GetAxis("VerticalR") * zoomStrength);
+        } else
+        {
+            joystickPos = new Vector3(Input.GetAxis("HorizontalR"), 0, Input.GetAxis("VerticalR")) * maxJoystickOffset;
+        }
         Vector3 finalPos = new Vector3(target.position.x, 0, target.position.z) + offsetPos + joystickPos;
         Vector3 smoothPos = Vector3.SmoothDamp(transform.position, finalPos,ref velocity, cameraSpeed);
         transform.position = smoothPos;
